10fd8317464b58cef4badca6a3ad763d6061d599
jiajun <jjxiong.hk@gmail.com>
2025-06-01 19:33:17 +0800

    update file path and default file path

--- src/main/java/com/benchmark/BenchmarkConstant.java
+++ src/main/java/com/benchmark/BenchmarkConstant.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public class BenchmarkConstant {
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
-    public static final String FILE_PATH = "src/main/resources/1/0700_HK.csv";
-    public static final String DEFAULT_FILE_PATH = "src/main/resources1/HK.csv";
+    public static final String FILE_PATH = "src/main/resources/0700_HK.csv";
+    public static final String DEFAULT_FILE_PATH = "src/main/resources/HK.csv";
     public static final int VALID_BUCKETS = 332; // HK volume profile buckets number
 }
